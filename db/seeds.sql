1e25315d1922406b0697fd7409466f75ec7c0351
George Arredondo <arredondo.george@gmail.com>
2019-04-10 17:28:22 -0500

    Set up my schema and seed files.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Patty Melt");
+INSERT INTO burgers (burger_name) VALUES ("In N Out Animal Style");
+INSERT INTO burgers (burger_name) VALUES ("Jalapeño Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Impossible Burger");
